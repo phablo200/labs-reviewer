15b9560bbedb92065961d6e2cc8b647537402022
Phablo Vilas Boas <phablovilasboas25@gmail.com>
2026-06-13 09:17:45 -0300

    feat(Config) - Adding new backend URLs to CORS.

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ FRONTEND_ORIGINS = [
     "http://127.0.0.1:4009",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 app.add_middleware(
